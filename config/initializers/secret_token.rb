6e6f0563701e2f80d217ab77cc33261845269133
nmt1402 <nmt1402@gmail.com>
2013-01-01 21:55:04 +0700

    Done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuck::Application.config.secret_token = 'b2581af1a02c6b68e5ea68a3551d326f932a2de000f9570e1b22d3873f199483873613149c0390b4d2ec40d3b383d1d0de67ac958d4da6e4601fc156bb71a236'
